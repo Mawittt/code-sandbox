996145decacc347cdeb9632ef5b4c202c83270b1
Mawittt <92302431+Mawittt@users.noreply.github.com>
2022-06-29 11:26:56 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <select>
+        <option>option one</option>
+        <option>option two</option>
+        <option>option three</option>
+      </select>
+    </div>
+  );
+}
